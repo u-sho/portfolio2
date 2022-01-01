c06c28cf39aa38d318a5f2dfeb3629d16909cc20
u-sho <shouhei.uechi@gmail.com>
2022-01-01 21:56:10 +0900

    🔥 Remove: todos/ path

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,1 +1,5 @@
 /// <reference types="@sveltejs/kit" />
+
+interface Locals {
+	userid: string;
+}
--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -26,18 +26,12 @@
 		following into your command line and following the prompts:
 	</p>
 
-	<!-- TODO lose the @next! -->
 	<pre>npm init svelte@next</pre>
 
 	<p>
 		The page you're looking at is purely static HTML, with no client-side interactivity needed.
 		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
 		the devtools network panel and reloading.
-	</p>
-
-	<p>
-		The <a href="/todos">TODOs</a> page illustrates SvelteKit's data loading and form handling. Try using
-		it with JavaScript disabled!
 	</p>
 </div>
 
