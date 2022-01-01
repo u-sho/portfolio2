92306f4b4614cccaf65b822e33fcad286a34289e
u-sho <shouhei.uechi@gmail.com>
2022-01-01 15:13:31 +0900

    🔧 add strict type check

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,27 @@
 module.exports = {
 	root: true,
 	parser: '@typescript-eslint/parser',
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:@typescript-eslint/recommended-requiring-type-checking'
+	],
 	plugins: ['svelte3', '@typescript-eslint'],
-	ignorePatterns: ['*.cjs'],
+	ignorePatterns: ['node_modules/', '.svelte-kit/', 'build/', 'package/'],
 	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
 	settings: {
 		'svelte3/typescript': () => require('typescript')
 	},
 	parserOptions: {
 		sourceType: 'module',
-		ecmaVersion: 2019
+		ecmaVersion: 2020,
+		tsconfigRootDir: __dirname,
+		project: ['./tsconfig.eslint.json'],
+		extraFileExtensions: ['.svelte', '.cjs']
 	},
 	env: {
 		browser: true,
-		es2017: true,
+		es2020: true,
 		node: true
 	}
 };
