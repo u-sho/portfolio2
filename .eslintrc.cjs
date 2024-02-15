474bba143cbd840cec3cb34a1c712356bcbe8a62
u-sho <26079835+u-sho@users.noreply.github.com>
2024-02-16 06:24:30 +0900

    fix eslint error

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,8 +4,8 @@ module.exports = {
 	parser: '@typescript-eslint/parser',
 	extends: [
 		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:@typescript-eslint/recommended-requiring-type-checking',
+		'plugin:@typescript-eslint/recommended-type-checked',
+		'plugin:@typescript-eslint/stylistic-type-checked',
 		'plugin:svelte/recommended',
 		'prettier'
 	],
@@ -17,10 +17,14 @@ module.exports = {
 			parser: 'svelte-eslint-parser',
 			parserOptions: {
 				parser: '@typescript-eslint/parser'
+			},
+			rules: {
+				'@typescript-eslint/no-floating-promises': 'off' // $: syntax
 			}
 		}
 	],
 	parserOptions: {
+		project: './tsconfig.eslint.json',
 		sourceType: 'module',
 		ecmaVersion: 2024,
 		extraFileExtensions: ['.svelte']
